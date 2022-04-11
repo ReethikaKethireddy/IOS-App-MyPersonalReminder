577ecfe68093258d2b4f214f8165a4f5a6edad3a
Reethika <reethikakethireddy@gmail.com>
2022-04-10 23:26:00 -0400

    Changed UITextField to UILabel. Reason. Upon hovering and clicking the UITextfield, by default keyboard is popping up and I was able to change the text .

--- My Personal Remainder/ViewController.swift
+++ My Personal Remainder/ViewController.swift
@@ -9,16 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var welcomePageLoaded: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var messageLabel1: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("😇Welcome!🙂!!")
+        print("😇viewdidloded🙂!!")
+        //messageLabel.text = "Are you ready to Personalize?"
     }
 
     @IBAction func messagePopUP(_ sender: UIButton) {
         print("Work in progress🙋🏻‍♀️")
+        
+        messageLabel1.text = "Hey Buddy! I'm here to help you. Login!"
     }
     
 }
