cbc5a5fc415f0cfcbc2a59a003615f54800c4e95
Reethika <reethikakethireddy@gmail.com>
2022-04-19 20:55:43 -0400

    ADDED A NEW VIEW CONTROLLER TO TAKE REMAINDER TITLE AND BODY ALONG WITH DATE PICKER.

--- My Personal Remainder/ViewController.swift
+++ My Personal Remainder/ViewController.swift
@@ -6,46 +6,159 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet var table: UITableView!
+
+    var models = [MyReminder]()
+    override func viewDidLoad()
+    {
     
-    @IBOutlet weak var messageLabel1: UILabel!
-    
-    override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("😇viewdidloded🙂!!")
+        table.delegate = self
+        table.dataSource = self
+        
     }
 
-    @IBAction func messagePopUP(_ sender: UIButton) {
-        print("Work in progress🙋🏻‍♀️")
-        //messageLabel.text = "Are you ready to Personalize?"
-        messageLabel1.text = "Hey Buddy! \n I'm here to help you!"
-    
-        func listRemainders(_ sender: UIButton)
+    @IBAction func didTapAdd()
+    {
+      
+        guard let vc = storyboard?.instantiateViewController(identifier: "add") as? vc2 else
         {
+            return
         }
-    
-    }
- 
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        //This code block is to launchscreen time and slow down app load time.
-        try! FileManager.default.removeItem(atPath: NSHomeDirectory()+"/Library/SplashBoard")
-        do {
-            sleep(9)
+
+        vc.title = "New Reminder"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.completion = { title, body, date in
+            DispatchQueue.main.async {
+                self.navigationController?.popToRootViewController(animated: true)
+                let new = MyReminder(title: title, date: date, identifier: "id_\(title)")
+                self.models.append(new)
+                self.table.reloadData()
+
+                let content = UNMutableNotificationContent()
+                content.title = title
+                content.sound = .default
+                content.body = body
+
+                let targetDate = date
+                let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate), repeats: false)
+
+                let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+                    if error != nil {
+                        print("something went wrong")
+                    }
+                })
+            }
         }
-        return true
-    }
-    
-    
-    struct MyReminder {
-        let title: String
-        let date: Date
-        let identifier: String
+        navigationController?.pushViewController(vc, animated: true)
+
     }
 
+//@IBAction func didTapTest(){}
+
+
+
+//   @IBAction func test() {
+//        // fire test notification
+//        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { success, error in
+//            if success {
+//                // schedule test
+//                self.scheduleTest()
+//            }
+//            else if error != nil {
+//                print("error occurred")
+//            }
+//        })
+//    }
+    @IBAction func test() {
+        // fire test notification
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { success, error in
+            if success {
+                // schedule test
+                self.scheduleTest()
+            }
+            else if error != nil {
+                print("error occurred")
+            }
+        })
+    }
     
+    func scheduleTest() {
+        let content = UNMutableNotificationContent()
+        content.title = "Hello World"
+        content.sound = .default
+        content.body = "My long body. My long body. My long body. My long body. My long body. My long body. "
+
+        let targetDate = Date().addingTimeInterval(10)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate), repeats: false)
+
+        let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print("something went wrong")
+            }
+        })
+    }
+
 }
+
+
+extension ViewController: UITableViewDelegate
+{
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
+
+
+extension ViewController: UITableViewDataSource
+{
+
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return models.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        let date = models[indexPath.row].date
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM, dd, YYYY"
+        cell.detailTextLabel?.text = formatter.string(from: date)
+
+        cell.textLabel?.font = UIFont(name: "Arial", size: 25)
+        cell.detailTextLabel?.font = UIFont(name: "Arial", size: 22)
+        return cell
+    }
+    
+    
+
+}
+
+
+ 
+struct MyReminder {
+       let title: String
+       let date: Date
+       let identifier: String
+}
+
+
+
